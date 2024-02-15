b433dc2a3b49f7e6d693c9e95b3fd58f208c2efb
OttoChu <96123119+OttoChu@users.noreply.github.com>
2024-02-15 21:19:56 +0000

    Added basic claim property actions

---
+++ app/properties/claim/actions.ts
@@ -0,0 +1,65 @@
+'use server'
+
+import { Database } from "@/supabase.types";
+import { createClient as createServerClient } from "@/utils/supabase/server";
+import { createClient } from '@supabase/supabase-js'
+import { cookies } from "next/headers";
+import { z } from "zod";
+
+export default function ClaimProperty() {
+    async function claimProperty(formData: FormData) {
+        const property_id = formData.get('property_id');
+
+        if (!property_id) {
+            return {
+                errors: {
+                    property_id: ['Property ID is required']
+                },
+                message: 'Invalid Fields Submitted'
+            }
+        }
+        // Need to check more things here before we can claim a property
+        
+        const cookieStore = cookies();
+        const supabase = createServerClient(cookieStore);
+
+        // check if the user is logged in
+        const { data: { user }, error: userError } = await supabase.auth.getUser()
+        if (!user) {
+            return {
+                message: 'User Not Logged In'
+            }
+        }
+
+        //  check if the user is already the landlord of the property
+        const { data: propertyOwnership, error: propertyOwnershipError } = await supabase
+            .from('property_ownership')
+            .select('landlord_id')
+            .match({ id: property_id })
+        if (propertyOwnership && propertyOwnership.length > 0 && propertyOwnership[0].landlord_id === user.id) {
+            return {
+                message: 'User is already the landlord of this property'
+            }
+        }
+
+
+        // update the property_ownership table
+        const user_id = user.id;
+
+        const { data, error } = await supabase
+            .from('property_ownership')
+            .update({
+                landlord_id: user_id,
+                // TODO: currently the started_at is set to the current date
+                //       maybe the landlord should be able to choose a date?
+                started_at: new Date().toISOString()
+            })
+            .match({ id: property_id })
+    }
+}
+
+// Things to discuss on 16/2/24 meeting:
+// 1. When do the ended_at get set? 
+//    - When a new landlord claims, set the ended_at of the previous landlord to the current date (-1 maybe?)
+//      If this is the case then what should ended_at be set to when the property is first claimed?
+//    - When claiming set a date and they choose a date?
---
+++ app/properties/claim/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function ClaimProperty() {
+    return (
+        <div>
+            <h1>Claim Property</h1>
+        </div>
+    )
+}
