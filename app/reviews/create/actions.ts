dab60e94d160f64efcdb58ad2b695a0405a81904
Luke Hoskins <74842299+LukeHosk@users.noreply.github.com>
2024-02-15 12:55:22 +0000

    Review Creation Backend (#5)
    * RRS-137 Setup review table
    * RRS-138 Setup reviewer tables
    * install zod & update typescript
    * RRS-139 Review creation for exisiting properties
    * RRS-139 Allow adding reviews for new properties
    * attempt to resolve type gen issues
    * clean migration history
    
    Sets up the database to allow reviews to be created and defines a server action to verify review contents before safely uploading it

---
+++ app/reviews/create/actions.ts
@@ -0,0 +1,174 @@
+'use server'
+
+import { Database } from "@/supabase.types";
+import { createClient as createServerClient } from "@/utils/supabase/server";
+import { createClient } from '@supabase/supabase-js'
+import { cookies } from "next/headers";
+import { z } from "zod";
+
+const newReviewSchema = z.object({
+    property_id: z.string().uuid().optional(),
+    property_address: z.string().optional(),
+
+    review_date: z.coerce.date(),
+
+    review_body: z.string().min(1).max(1000),
+    property_rating: z.coerce.number().int().min(1).max(5),
+    landlord_rating: z.coerce.number().int().min(1).max(5),
+})
+
+export type State = {
+    errors?: {
+        property_id?: string[]
+        property_address?: string[],
+        review_date?: string[],
+        review_body?: string[],
+        property_rating?: string[],
+        landlord_rating?: string[]
+    };
+    message?: string | null;
+};
+
+//given either the id of an existing property or the address of a new one, creates a review written by the currently logged in user
+export const createReview = async (
+    property : {
+        id: string | undefined, 
+        address: string | undefined,
+    },
+    prevState: State,
+    formData: FormData
+) : Promise<State> => {
+    // Validate input
+    const validatedFields = newReviewSchema.safeParse({
+        property_id: property.id,
+        property_address: property.address,
+        review_date: formData.get('review_date'),
+        review_body: formData.get('review_body'),
+        property_rating: formData.get('property_rating'),
+        landlord_rating: formData.get('landlord_rating'),
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Invalid Fields Submitted'
+        }
+    }
+
+    let {
+        property_id,
+        property_address,
+        review_date,
+        review_body,
+        property_rating,
+        landlord_rating
+    } = validatedFields.data;
+
+    // create service client
+    const serviceSupabase = createClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.SUPABASE_SERVICE_KEY!
+    )
+
+    if (!property_id) {
+        if (!property_address) {
+            return {
+                message: 'No Property Selected'
+            }
+        }
+
+        // TODO: Handle property creation
+
+        // Check if property with address exists
+        const { data: existingProperty, error: existingPropertyError } = await serviceSupabase
+            .from('properties')
+            .select('id')
+            .eq('address', property_address)
+            .maybeSingle()
+        // If it does - Error
+
+        if (existingProperty) {
+            return {
+                message: 'Property Already Exists'
+            }
+        }
+
+        // If it doesn't - Create Property & set property_id
+        const { data: newProperty, error: newPropertyError } = await serviceSupabase
+            .from('properties')
+            .insert({ address: property_address })
+            .select('id')
+            .single()
+
+        if (newPropertyError) {
+            return {
+                message: 'Error Creating Property'
+            }
+        }
+
+        property_id = newProperty.id // replace with actual property_id
+    }
+
+    // get user
+    const cookieStore = cookies()
+    const supabase = createServerClient(cookieStore)
+
+    const { data: { user }, error: userError } = await supabase.auth.getUser()
+    if (!user) {
+        return {
+            message: 'User Not Logged In'
+        }
+    }
+
+    const { id: user_id } = user
+
+    // check that the user has not already reviewed the property
+    const { data: existingReview, error: existingReviewError } = await supabase
+        .from('reviewer_private_profiles')
+        .select('*')
+        .eq('property_id', property_id)
+        .eq('user_id', user_id)
+        .maybeSingle()
+
+    if (existingReview) {
+        return {
+            message: 'User has already reviewed this property'
+        }
+    }
+
+    // all good, create review
+
+    // create reviewer profile
+    const { data: reviewerProfile, error: reviewerProfileError } = await serviceSupabase
+        .from('reviewer_private_profiles')
+        .insert({
+            user_id,
+            property_id,
+        })
+        .select()
+        .single()
+
+    if (reviewerProfileError) {
+        return {
+            message: 'Error Creating Reviewer Profile'
+        }
+    }
+
+    const { reviewer_id } = reviewerProfile
+
+    // create reviews
+    const { data: review, error: reviewError } = await serviceSupabase
+        .from('reviews')
+        .insert({
+            landlord_rating,
+            review_date: review_date.toISOString(),
+            property_id,
+            reviewer_id,
+            review_body,
+            property_rating,
+        })
+
+    return {
+        message: 'Review Created'
+    }
+}
--- supabase.types.ts
+++ supabase.types.ts
@@ -48,6 +48,7 @@ export interface Database {
           {
             foreignKeyName: "landlord_private_profiles_id_fkey"
             columns: ["id"]
+            isOneToOne: true
             referencedRelation: "user_profiles"
             referencedColumns: ["id"]
           }
@@ -67,6 +68,7 @@ export interface Database {
           {
             foreignKeyName: "landlord_public_profiles_id_fkey"
             columns: ["id"]
+            isOneToOne: true
             referencedRelation: "landlord_private_profiles"
             referencedColumns: ["id"]
           }
@@ -74,12 +76,15 @@ export interface Database {
       }
       properties: {
         Row: {
+          address: string
           id: string
         }
         Insert: {
+          address: string
           id?: string
         }
         Update: {
+          address?: string
           id?: string
         }
         Relationships: []
@@ -107,12 +112,14 @@ export interface Database {
           {
             foreignKeyName: "property_ownership_landlord_id_fkey"
             columns: ["landlord_id"]
+            isOneToOne: false
             referencedRelation: "landlord_public_profiles"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "property_ownership_property_id_fkey"
             columns: ["property_id"]
+            isOneToOne: false
             referencedRelation: "properties"
             referencedColumns: ["id"]
           }
@@ -135,12 +142,14 @@ export interface Database {
           {
             foreignKeyName: "review_photos_photo_id_fkey"
             columns: ["photo_id"]
+            isOneToOne: false
             referencedRelation: "uploaded_files"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "review_photos_review_id_fkey"
             columns: ["review_id"]
+            isOneToOne: false
             referencedRelation: "reviews"
             referencedColumns: ["review_id"]
           }
@@ -166,12 +175,14 @@ export interface Database {
           {
             foreignKeyName: "reviewer_private_profiles_property_id_fkey"
             columns: ["property_id"]
+            isOneToOne: false
             referencedRelation: "properties"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "reviewer_private_profiles_user_id_fkey"
             columns: ["user_id"]
+            isOneToOne: false
             referencedRelation: "user_profiles"
             referencedColumns: ["id"]
           }
@@ -209,12 +220,14 @@ export interface Database {
           {
             foreignKeyName: "reviews_property_id_fkey"
             columns: ["property_id"]
+            isOneToOne: false
             referencedRelation: "properties"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "reviews_reviewer_id_fkey"
             columns: ["reviewer_id"]
+            isOneToOne: false
             referencedRelation: "reviewer_private_profiles"
             referencedColumns: ["reviewer_id"]
           }
@@ -261,12 +274,14 @@ export interface Database {
           {
             foreignKeyName: "user_profiles_id_fkey"
             columns: ["id"]
+            isOneToOne: true
             referencedRelation: "users"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "user_public_profiles_id_fkey"
             columns: ["id"]
+            isOneToOne: true
             referencedRelation: "users"
             referencedColumns: ["id"]
           }
@@ -297,6 +312,7 @@ export interface Database {
           id: string
           name: string
           owner: string | null
+          owner_id: string | null
           public: boolean | null
           updated_at: string | null
         }
@@ -308,6 +324,7 @@ export interface Database {
           id: string
           name: string
           owner?: string | null
+          owner_id?: string | null
           public?: boolean | null
           updated_at?: string | null
         }
@@ -319,17 +336,11 @@ export interface Database {
           id?: string
           name?: string
           owner?: string | null
+          owner_id?: string | null
           public?: boolean | null
           updated_at?: string | null
         }
-        Relationships: [
-          {
-            foreignKeyName: "buckets_owner_fkey"
-            columns: ["owner"]
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
+        Relationships: []
       }
       migrations: {
         Row: {
@@ -361,6 +372,7 @@ export interface Database {
           metadata: Json | null
           name: string | null
           owner: string | null
+          owner_id: string | null
           path_tokens: string[] | null
           updated_at: string | null
           version: string | null
@@ -373,6 +385,7 @@ export interface Database {
           metadata?: Json | null
           name?: string | null
           owner?: string | null
+          owner_id?: string | null
           path_tokens?: string[] | null
           updated_at?: string | null
           version?: string | null
@@ -385,6 +398,7 @@ export interface Database {
           metadata?: Json | null
           name?: string | null
           owner?: string | null
+          owner_id?: string | null
           path_tokens?: string[] | null
           updated_at?: string | null
           version?: string | null
@@ -393,6 +407,7 @@ export interface Database {
           {
             foreignKeyName: "objects_bucketId_fkey"
             columns: ["bucket_id"]
+            isOneToOne: false
             referencedRelation: "buckets"
             referencedColumns: ["id"]
           }
@@ -466,4 +481,84 @@ export interface Database {
     }
   }
 }
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
 
