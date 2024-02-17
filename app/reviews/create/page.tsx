81441320013243418c91b03626a70a08d5847ec2
Ailam7 <mo.ailam777@gmail.com>
2024-02-17 12:08:42 +0000

    Added Appropriate Fields to Create Review page according to prototypes

--- app/reviews/create/page.tsx
+++ app/reviews/create/page.tsx
@@ -1,74 +1,143 @@
-'use client'
+"use client";
 
 // TEST PAGE FOR CREATE ACTION - PLEASE REPLACE
-import { useFormState } from 'react-dom'
+import { useFormState } from "react-dom";
 import { createReview } from "./actions";
 
 const createReviewPage = () => {
   // define example propertyId and address, these should be determined by params in implementation
-  const examplePropertyId = undefined // '7cda6a4c-868d-436d-a05b-25d6eeaed351'
-  const exampleAddress = "1234 Example St, Example, EX 12345";
+  const examplePropertyId = undefined; // '7cda6a4c-868d-436d-a05b-25d6eeaed351'
+  const exampleAddress = "1 Lorem Ipsum Road, Oldfield Park"; // ofc replace this later
 
   // bind the id & address to the createReview function
   const initialState = { message: null, errors: {} };
-  const createReviewWithProperty = createReview.bind(null, {id: examplePropertyId, address: exampleAddress});
-  const [state, dispatch] = useFormState(createReviewWithProperty, initialState)
+  const createReviewWithProperty = createReview.bind(null, {
+    id: examplePropertyId,
+    address: exampleAddress,
+  });
+  const [state, dispatch] = useFormState(
+    createReviewWithProperty,
+    initialState
+  );
 
   return (
-    <div>
-      <h1>Create Review</h1>
-      <p>Test page for create review action - REPLACE ME</p>
+    <div style={{ minWidth: "1000px" }}>
+      <h1 className="text-4xl font-bold mb-2 mt-8">Add New Review</h1>
+      <p className="mb-8 text-2l">
+        Add a new review for this property:{" "}
+        <span>1 Lorem Ipsum Road, Oldfield Park</span>
+      </p>
 
       <form
         className="flex flex-col gap-2 pleace-items-center max-w-prose w-full border p-4 rounded-md"
         action={dispatch}
       >
-        <label htmlFor="review_date">Review Date</label>
-        <input 
+        <h2 className="text-xl font-semibold">Your Details</h2>
+
+        <label htmlFor="name">Your Name</label>
+        <textarea
+          className="border p-1 rounded-md h-10"
+          name="name"
+          id="name"
+          required
+        ></textarea>
+
+        <label htmlFor="tenancy_period">Tenancy Period</label>
+        <input
           className="border p-2 rounded-md"
-          type="date"
-          name="review_date"
-          id="review_date"
+          type="text"
+          name="tenancy_period"
+          id="tenancy_period"
           required
         />
 
-        <label htmlFor="review_body">Review Body</label>
-        <textarea 
+        <label htmlFor="proof_of_tenancy">Upload proof of tenancy</label>
+        <input
+          className="border p-2 rounded-md mb-6"
+          type="file"
+          name="proof_of_tenancy"
+          id="proof_of_tenancy"
+          accept="image/*"
+          required
+        />
+
+        <h2 className="text-xl font-semibold">Landlord Ratings</h2>
+
+        <label htmlFor="speed_rating">Speed to Reply</label>
+        <input
           className="border p-2 rounded-md"
+          type="number"
+          name="speed_rating"
+          id="speed_rating"
+          min="1"
+          max="5"
+          required
+        />
+
+        <label htmlFor="contract_fairness_rating">Fairness of contract</label>
+        <input
+          className="border p-2 rounded-md"
+          type="number"
+          name="contract_fairness_rating"
+          id="contract_fairness_rating"
+          min="1"
+          max="5"
+          required
+        />
+
+        <label htmlFor="friendliness_rating">Friendliness</label>
+        <input
+          className="border p-2 rounded-md mb-6"
+          type="number"
+          name="friendliness_rating"
+          id="friendliness_rating"
+          min="1"
+          max="5"
+          required
+        />
+
+        <h2 className="text-xl font-semibold">Overall Ratings</h2>
+
+        <label htmlFor="friendliness_rating">Overall Rating</label>
+        <input
+          className="border p-2 rounded-md"
+          type="number"
+          name="friendliness_rating"
+          id="friendliness_rating"
+          min="1"
+          max="5"
+          required
+        />
+
+        <label htmlFor="property_images">Upload images of property here</label>
+        <input
+          className="border p-2 rounded-md"
+          type="file"
+          name="property_images"
+          id="property_images"
+          accept="image/*"
+          required
+        />
+
+        <label htmlFor="review_body">
+          Write your review here (max 300 characters)
+        </label>
+        <textarea
+          className="border p-2 rounded-md h-40"
           name="review_body"
           id="review_body"
           required
         ></textarea>
 
-        <label htmlFor="property_rating">Property Rating</label>
-        <input 
-          className="border p-2 rounded-md"
-          type="number"
-          name="property_rating"
-          id="property_rating"
-          min="1"
-          max="5"
-          required
-        />
-
-        <label htmlFor="landlord_rating">Landlord Rating</label>
-        <input 
-          className="border p-2 rounded-md"
-          type="number"
-          name="landlord_rating"
-          id="landlord_rating"
-          min="1"
-          max="5"
-          required
-        />
-
-        <button 
+        <button
           className="rounded-md p-2 hover:bg-gray-600/20 border"
           type="submit"
-        >Create Review</button>
+        >
+          Create Review
+        </button>
 
         {state.message && <p>{state.message}</p>}
-        {state.errors && <p>{JSON.stringify(state.errors, null, '\t')}</p>}
+        {state.errors && <p>{JSON.stringify(state.errors, null, "\t")}</p>}
       </form>
     </div>
   );
