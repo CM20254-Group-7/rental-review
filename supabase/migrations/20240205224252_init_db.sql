af1ff45d5bf6a6d240e627cdbefeeffbe4d6f14a
Euan <euand1818@gmail.com>
2024-02-18 18:11:45 +0000

    changed back to id

--- supabase/migrations/20240205224252_init_db.sql
+++ supabase/migrations/20240205224252_init_db.sql
@@ -1,5 +1,5 @@
 create table "public"."landlord_private_profiles" (
-    "user_id" uuid not null,
+    "id" uuid not null,
     "phone_number" varchar(20) not null,
     "postcode" varchar(7) not null,
     "country" varchar(20) not null,
@@ -13,7 +13,7 @@ create table "public"."landlord_private_profiles" (
 alter table "public"."landlord_private_profiles" enable row level security;
 
 create table "public"."landlord_public_profiles" (
-    "user_id" uuid not null,
+    "id" uuid not null,
     "website" varchar(50),
     "bio" varchar(200),
     "profile_image_id" uuid,
@@ -60,7 +60,7 @@ create table "public"."reviewer_private_profiles" (
 alter table "public"."reviewer_private_profiles" enable row level security;
 
 create table "public"."reviewer_public_profiles" (
-    "id" uuid not null,
+    "user_id" uuid not null,
     "created_at" timestamp with time zone not null default now()
 );
 
@@ -86,7 +86,7 @@ create table "public"."uploaded_files" (
 alter table "public"."uploaded_files" enable row level security;
 
 create table "public"."user_profiles" (
-    "id" uuid not null,
+    "user_id" uuid not null,
     "first_name" text,
     "last_name" text,
     "email" text,
@@ -96,9 +96,9 @@ create table "public"."user_profiles" (
 
 alter table "public"."user_profiles" enable row level security;
 
-CREATE UNIQUE INDEX landlord_private_profiles_pkey ON public.landlord_private_profiles USING btree (user_id);
+CREATE UNIQUE INDEX landlord_private_profiles_pkey ON public.landlord_private_profiles USING btree (id);
 
-CREATE UNIQUE INDEX landlord_public_profiles_pkey ON public.landlord_public_profiles USING btree (user_id);
+CREATE UNIQUE INDEX landlord_public_profiles_pkey ON public.landlord_public_profiles USING btree (id);
 
 CREATE UNIQUE INDEX properties_pkey ON public.properties USING btree (id);
 
@@ -112,7 +112,7 @@ CREATE UNIQUE INDEX reviewer_ids_reviewer_id_key ON public.reviewer_private_prof
 
 CREATE UNIQUE INDEX reviewer_private_profiles_user_id_property_id_key ON public.reviewer_private_profiles USING btree (user_id, property_id);
 
-CREATE UNIQUE INDEX reviewer_public_profiles_pkey ON public.reviewer_public_profiles USING btree (id);
+CREATE UNIQUE INDEX reviewer_public_profiles_pkey ON public.reviewer_public_profiles USING btree (user_id);
 
 CREATE UNIQUE INDEX reviews_pkey ON public.reviews USING btree (review_id);
 
@@ -120,7 +120,7 @@ CREATE UNIQUE INDEX reviews_property_id_reviewer_id_key ON public.reviews USING 
 
 CREATE UNIQUE INDEX uploaded_files_pkey ON public.uploaded_files USING btree (id);
 
-CREATE UNIQUE INDEX user_public_profiles_pkey ON public.user_profiles USING btree (id);
+CREATE UNIQUE INDEX user_public_profiles_pkey ON public.user_profiles USING btree (user_id);
 
 alter table "public"."landlord_private_profiles" add constraint "landlord_private_profiles_pkey" PRIMARY KEY using index "landlord_private_profiles_pkey";
 
@@ -142,62 +142,41 @@ alter table "public"."uploaded_files" add constraint "uploaded_files_pkey" PRIMA
 
 alter table "public"."user_profiles" add constraint "user_public_profiles_pkey" PRIMARY KEY using index "user_public_profiles_pkey";
 
-alter table "public"."landlord_private_profiles" add constraint "landlord_private_profiles_id_fkey" FOREIGN KEY (user_id) REFERENCES user_profiles(id) not valid;
-
+alter table "public"."landlord_private_profiles" add constraint "landlord_private_profiles_id_fkey" FOREIGN KEY (id) REFERENCES user_profiles(user_id) not valid;
 alter table "public"."landlord_private_profiles" validate constraint "landlord_private_profiles_id_fkey";
 
-alter table "public"."landlord_public_profiles" add constraint "landlord_public_profiles_id_fkey" FOREIGN KEY (user_id) REFERENCES landlord_private_profiles(user_id) not valid;
-
+alter table "public"."landlord_public_profiles" add constraint "landlord_public_profiles_id_fkey" FOREIGN KEY (id) REFERENCES landlord_private_profiles(id) not valid;
 alter table "public"."landlord_public_profiles" validate constraint "landlord_public_profiles_id_fkey";
 
-alter table "public"."property_ownership" add constraint "property_ownership_landlord_id_fkey" FOREIGN KEY (landlord_id) REFERENCES landlord_public_profiles(user_id) not valid;
-
+alter table "public"."property_ownership" add constraint "property_ownership_landlord_id_fkey" FOREIGN KEY (landlord_id) REFERENCES landlord_public_profiles(id) not valid;
 alter table "public"."property_ownership" validate constraint "property_ownership_landlord_id_fkey";
-
 alter table "public"."property_ownership" add constraint "property_ownership_property_id_fkey" FOREIGN KEY (property_id) REFERENCES properties(id) not valid;
-
 alter table "public"."property_ownership" validate constraint "property_ownership_property_id_fkey";
 
 alter table "public"."review_photos" add constraint "review_photos_photo_id_fkey" FOREIGN KEY (photo_id) REFERENCES uploaded_files(id) not valid;
-
 alter table "public"."review_photos" validate constraint "review_photos_photo_id_fkey";
-
 alter table "public"."review_photos" add constraint "review_photos_review_id_fkey" FOREIGN KEY (review_id) REFERENCES reviews(review_id) not valid;
-
 alter table "public"."review_photos" validate constraint "review_photos_review_id_fkey";
 
 alter table "public"."reviewer_private_profiles" add constraint "reviewer_ids_reviewer_id_key" UNIQUE using index "reviewer_ids_reviewer_id_key";
-
 alter table "public"."reviewer_private_profiles" add constraint "reviewer_private_profiles_property_id_fkey" FOREIGN KEY (property_id) REFERENCES properties(id) not valid;
-
 alter table "public"."reviewer_private_profiles" validate constraint "reviewer_private_profiles_property_id_fkey";
-
-alter table "public"."reviewer_private_profiles" add constraint "reviewer_private_profiles_user_id_fkey" FOREIGN KEY (user_id) REFERENCES user_profiles(id) not valid;
-
+alter table "public"."reviewer_private_profiles" add constraint "reviewer_private_profiles_user_id_fkey" FOREIGN KEY (user_id) REFERENCES user_profiles(user_id) not valid;
 alter table "public"."reviewer_private_profiles" validate constraint "reviewer_private_profiles_user_id_fkey";
-
 alter table "public"."reviewer_private_profiles" add constraint "reviewer_private_profiles_user_id_property_id_key" UNIQUE using index "reviewer_private_profiles_user_id_property_id_key";
 
-alter table "public"."reviewer_public_profiles" add constraint "reviewer_public_profiles_id_fkey" FOREIGN KEY (id) REFERENCES reviewer_private_profiles(reviewer_id) not valid;
-
+alter table "public"."reviewer_public_profiles" add constraint "reviewer_public_profiles_id_fkey" FOREIGN KEY (user_id) REFERENCES reviewer_private_profiles(reviewer_id) not valid;
 alter table "public"."reviewer_public_profiles" validate constraint "reviewer_public_profiles_id_fkey";
 
 alter table "public"."reviews" add constraint "reviews_property_id_fkey" FOREIGN KEY (property_id) REFERENCES properties(id) not valid;
-
 alter table "public"."reviews" validate constraint "reviews_property_id_fkey";
-
 alter table "public"."reviews" add constraint "reviews_property_id_reviewer_id_key" UNIQUE using index "reviews_property_id_reviewer_id_key";
-
-alter table "public"."reviews" add constraint "reviews_reviewer_id_fkey" FOREIGN KEY (reviewer_id) REFERENCES reviewer_public_profiles(id) not valid;
-
+alter table "public"."reviews" add constraint "reviews_reviewer_id_fkey" FOREIGN KEY (reviewer_id) REFERENCES reviewer_public_profiles(user_id) not valid;
 alter table "public"."reviews" validate constraint "reviews_reviewer_id_fkey";
 
-alter table "public"."user_profiles" add constraint "user_profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) not valid;
-
+alter table "public"."user_profiles" add constraint "user_profiles_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id) not valid;
 alter table "public"."user_profiles" validate constraint "user_profiles_id_fkey";
-
-alter table "public"."user_profiles" add constraint "user_public_profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) not valid;
-
+alter table "public"."user_profiles" add constraint "user_public_profiles_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id) not valid;
 alter table "public"."user_profiles" validate constraint "user_public_profiles_id_fkey";
 
 
