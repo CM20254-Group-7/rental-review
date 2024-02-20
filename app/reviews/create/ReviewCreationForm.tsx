db85ea5d7aabfded177c593646302d461a93c1b0
Ailam7 <mo.ailam777@gmail.com>
2024-02-20 17:02:44 +0000

    Made the appropriate changes: Removed specific landlord fields Added option for a property rating Added option for a review title Modifed actions.ts to include review title

--- app/reviews/create/ReviewCreationForm.tsx
+++ app/reviews/create/ReviewCreationForm.tsx
@@ -1,140 +1,127 @@
-'use client'
+"use client";
 
 import { useFormState } from "react-dom";
 import { createReview } from "./actions";
 
 interface FormProps {
-    propertyId?: string,
-    address?: string,
+  propertyId?: string;
+  address?: string;
 }
 export const ReviewCreationForm: React.FC<FormProps> = ({
-    propertyId,
-    address = "1 Lorem Ipsum Road, Oldfield Park"  // For Testing, add dummy value if none provided, consider replacing with error handling
+  propertyId,
+  address = "1 Lorem Ipsum Road, Oldfield Park", // For Testing, add dummy value if none provided, consider replacing with error handling
 }) => {
+  // bind the id & address to the createReview function
+  const initialState = { message: null, errors: {} };
+  const createReviewWithProperty = createReview.bind(null, {
+    id: propertyId,
+    address: address,
+  });
+  const [state, dispatch] = useFormState(
+    createReviewWithProperty,
+    initialState
+  );
 
-    // bind the id & address to the createReview function
-    const initialState = { message: null, errors: {} };
-    const createReviewWithProperty = createReview.bind(null, {
-        id: propertyId,
-        address: address,
-    });
-    const [state, dispatch] = useFormState(
-        createReviewWithProperty,
-        initialState
-    );
+  return (
+    <form
+      className="flex flex-col gap-2 pleace-items-center w-full border p-4 rounded-md mb-8"
+      action={dispatch}
+      style={{ backgroundColor: "#778378", color: "#222" }}
+    >
+      <h2 className="text-xl font-semibold">Your Details</h2>
 
-    return (
-        <form
-            className="flex flex-col gap-2 pleace-items-center w-full border p-4 rounded-md mb-8"
-            action={dispatch}
-            style={{ backgroundColor: "#778378", color: "#222" }}
-        >
-            <h2 className="text-xl font-semibold">Your Details</h2>
+      <label htmlFor="name">Your Name</label>
+      <textarea
+        className="border p-1 rounded-md h-10"
+        name="name"
+        id="name"
+        required
+      ></textarea>
 
-            <label htmlFor="name">Your Name</label>
-            <textarea
-                className="border p-1 rounded-md h-10"
-                name="name"
-                id="name"
-                required
-            ></textarea>
+      <label htmlFor="review_date">Date of review</label>
+      <input
+        className="border p-2 rounded-md"
+        type="date"
+        name="review_date"
+        id="review_date"
+        required
+      />
 
-            <label htmlFor="tenancy_period">Tenancy Period</label>
-            <input
-                className="border p-2 rounded-md"
-                type="text"
-                name="tenancy_period"
-                id="tenancy_period"
-                required
-            />
+      <label htmlFor="tenancy_period">Tenancy Period</label>
+      <input
+        className="border p-2 rounded-md mb-6"
+        type="text"
+        name="tenancy_period"
+        id="tenancy_period"
+        required
+      />
 
-            <label htmlFor="proof_of_tenancy">Upload proof of tenancy</label>
-            <input
-                className="border p-2 rounded-md mb-6"
-                type="file"
-                name="proof_of_tenancy"
-                id="proof_of_tenancy"
-                accept="image/*"
-                required
-            />
+      <h2 className="text-xl font-semibold">Ratings</h2>
 
-            <h2 className="text-xl font-semibold">Landlord Ratings</h2>
+      <label htmlFor="landlord_rating">Landlord Ratings</label>
+      <input
+        className="border p-2 rounded-md"
+        type="number"
+        name="landlord_rating"
+        id="landlord_rating"
+        min="1"
+        max="5"
+        required
+      />
 
-            <label htmlFor="speed_rating">Speed to Reply</label>
-            <input
-                className="border p-2 rounded-md"
-                type="number"
-                name="speed_rating"
-                id="speed_rating"
-                min="1"
-                max="5"
-                required
-            />
-            <label htmlFor="contract_fairness_rating">Fairness of contract</label>
-            <input
-                className="border p-2 rounded-md"
-                type="number"
-                name="contract_fairness_rating"
-                id="contract_fairness_rating"
-                min="1"
-                max="5"
-                required
-            />
+      <label htmlFor="property_rating">Property Rating</label>
+      <input
+        className="border p-2 rounded-md mb-6"
+        type="number"
+        name="property_rating"
+        id="property_rating"
+        min="1"
+        max="5"
+        required
+      />
 
-            <label htmlFor="friendliness_rating">Friendliness</label>
-            <input
-                className="border p-2 rounded-md mb-6"
-                type="number"
-                name="friendliness_rating"
-                id="friendliness_rating"
-                min="1"
-                max="5"
-                required
-            />
+      <h2 className="text-xl font-semibold">Write your review</h2>
 
-            <h2 className="text-xl font-semibold">Overall Ratings</h2>
+      {/* Commented out for now as file uploards are being disputed */}
+      {/* <label htmlFor="property_images">Upload images of property here</label>
+      <input
+        className="border p-2 rounded-md"
+        type="file"
+        name="property_images"
+        id="property_images"
+        accept="image/*"
+        required
+      /> */}
 
-            <label htmlFor="friendliness_rating">Overall Rating</label>
-            <input
-                className="border p-2 rounded-md"
-                type="number"
-                name="friendliness_rating"
-                id="friendliness_rating"
-                min="1"
-                max="5"
-                required
-            />
+      <label htmlFor="review_title">Review Title</label>
+      <input
+        className="border p-2 rounded-md"
+        type="text"
+        name="review_title"
+        id="review_title"
+        required
+      />
 
-            <label htmlFor="property_images">Upload images of property here</label>
-            <input
-                className="border p-2 rounded-md"
-                type="file"
-                name="property_images"
-                id="property_images"
-                accept="image/*"
-                required
-            />
+      <label htmlFor="review_body">
+        Write your review here (max 300 characters)
+      </label>
+      <textarea
+        className="border p-2 rounded-md h-40"
+        name="review_body"
+        id="review_body"
+        required
+      ></textarea>
 
-            <label htmlFor="review_body">
-                Write your review here (max 300 characters)
-            </label>
-            <textarea
-                className="border p-2 rounded-md h-40"
-                name="review_body"
-                id="review_body"
-                required
-            ></textarea>
+      <button
+        className="rounded-md p-2 border"
+        type="submit"
+        style={{ backgroundColor: "#E2B758" }}
+      >
+        Create Review
+      </button>
 
-            <button
-                className="rounded-md p-2 border"
-                type="submit"
-                style={{ backgroundColor: "#E2B758" }}
-            >
-                Create Review
-            </button>
-
-            {state.message && <p>{state.message}</p>}
-            {state.errors && <p>{JSON.stringify(state.errors, null, "\t")}</p>}
-        </form>
-    )
-}
+      {state.message && <p>{state.message}</p>}
+      {state.errors && <p>{JSON.stringify(state.errors, null, "\t")}</p>}
+    </form>
+  );
+};
--- app/reviews/create/page.tsx
+++ app/reviews/create/page.tsx
@@ -1,86 +1,44 @@
-"use client";
-
-// TEST PAGE FOR CREATE ACTION - PLEASE REPLACE
-import { useFormState } from "react-dom";
-import { createReview } from "./actions";
 import { NextPage } from "next";
 import { createClient } from "@/utils/supabase/client";
 import { ReviewCreationForm } from "./ReviewCreationForm";
 
-const createReviewPage = () => {
-  // define example propertyId and address, these should be determined by params in implementation
-  const examplePropertyId = undefined; // '7cda6a4c-868d-436d-a05b-25d6eeaed351'
-  const exampleAddress = "1234 Example St, Example, EX 12345";
+const createReviewPage: NextPage = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    propertyId?: string;
+    address?: string;
+  };
+}) => {
+  const propertyId = searchParams?.propertyId;
+  let address: string | undefined = undefined;
 
-  // bind the id & address to the createReview function
-  const initialState = { message: null, errors: {} };
-  const createReviewWithProperty = createReview.bind(null, {
-    id: examplePropertyId,
-    address: exampleAddress,
-  });
-  const [state, dispatch] = useFormState(
-    createReviewWithProperty,
-    initialState
-  );
+  // get the address from the propertyId if it's defined
+  if (propertyId) {
+    const supabase = createClient();
+
+    const { data, error } = await supabase
+      .from("properties")
+      .select("address")
+      .eq("id", propertyId)
+      .maybeSingle();
+
+    // Consider adding error handling here
+    // redirect to / show not-found if no property found with the provided id
+
+    address = data?.address;
+  }
+
+  if (!address) address = searchParams?.address;
 
   return (
-    <div>
-      <h1>Create Review</h1>
-      <p>Test page for create review action - REPLACE ME</p>
+    <div style={{ minWidth: "1000px" }}>
+      <h1 className="text-4xl font-bold mb-2 mt-8">Add New Review</h1>
+      <p className="mb-8 text-xl">
+        Add a new review for this property: <span>{address}</span>
+      </p>
 
-      <form
-        className="flex flex-col gap-2 pleace-items-center max-w-prose w-full border p-4 rounded-md"
-        action={dispatch}
-      >
-        <label htmlFor="review_date">Review Date</label>
-        <input
-          className="border p-2 rounded-md"
-          type="date"
-          name="review_date"
-          id="review_date"
-          required
-        />
-
-        <label htmlFor="review_body">Review Body</label>
-        <textarea
-          className="border p-2 rounded-md"
-          name="review_body"
-          id="review_body"
-          required
-        ></textarea>
-
-        <label htmlFor="property_rating">Property Rating</label>
-        <input
-          className="border p-2 rounded-md"
-          type="number"
-          name="property_rating"
-          id="property_rating"
-          min="1"
-          max="5"
-          required
-        />
-
-        <label htmlFor="landlord_rating">Landlord Rating</label>
-        <input
-          className="border p-2 rounded-md"
-          type="number"
-          name="landlord_rating"
-          id="landlord_rating"
-          min="1"
-          max="5"
-          required
-        />
-
-        <button
-          className="rounded-md p-2 hover:bg-gray-600/20 border"
-          type="submit"
-        >
-          Create Review
-        </button>
-
-        {state.message && <p>{state.message}</p>}
-        {state.errors && <p>{JSON.stringify(state.errors, null, "\t")}</p>}
-      </form>
+      <ReviewCreationForm propertyId={propertyId} address={address} />
     </div>
   );
 };
