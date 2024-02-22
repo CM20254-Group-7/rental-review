f192090046aa2e59069296e9db001a5e2cd1628d
Luke Hoskins <luke@hoskins.cloud>
2024-02-22 10:28:32 +0000

    create new user profile migration (with the trigger this time)

---
+++ supabase/migrations/20240222092851_new_user_profiles_v2.sql
@@ -0,0 +1,26 @@
+check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.create_profile_for_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$begin
+  insert into public.user_profiles(user_id, email)
+  values(new.id, new.email);
+  
+  return new;
+end;$function$
+;
+
+CREATE TRIGGER create_profile_for_new_user AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION create_profile_for_user();
+
+
+create policy "Allow users to get their own profile"
+on "public"."user_profiles"
+as permissive
+for select
+to public
+using ((auth.uid() = user_id));
+
+
+
