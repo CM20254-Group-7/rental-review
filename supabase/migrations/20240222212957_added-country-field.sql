e4a365e1d66aec66467d9320298b78be43270fb9
euand1818 <ed768@bath.ac.uk>
2024-02-22 22:36:24 +0000

    Added all fields to migration

--- supabase/migrations/20240222212957_added-country-field.sql
+++ supabase/migrations/20240222212957_added-country-field.sql
@@ -1,3 +1,17 @@
-alter table "public"."properties" add column "country" text;
+alter table "public"."properties" add column "baths" smallint; 
 
+alter table "public"."properties" add column "beds" smallint; 
 
+alter table "public"."properties" add column "country" text not null; 
+
+alter table "public"."properties" add column "county" text not null; 
+
+alter table "public"."properties" add column "description" text; 
+
+alter table "public"."properties" add column "house" text not null; 
+
+alter table "public"."properties" add column "postcode" text not null; 
+
+alter table "public"."properties" add column "property_type" text not null; 
+
+alter table "public"."properties" add column "street" text; 
