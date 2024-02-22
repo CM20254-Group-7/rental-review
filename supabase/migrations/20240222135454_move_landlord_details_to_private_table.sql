0382a071ff4993a39b15226bc718d2919c1f4acc
Luke Hoskins <luke@hoskins.cloud>
2024-02-22 16:00:51 +0000

    fields already removed from user table

--- supabase/migrations/20240222135454_move_landlord_details_to_private_table.sql
+++ supabase/migrations/20240222135454_move_landlord_details_to_private_table.sql
@@ -6,8 +6,3 @@ alter table "public"."landlord_public_profiles" add column "display_email" text 
 
 alter table "public"."landlord_public_profiles" add column "display_name" text not null;
 
-alter table "public"."user_profiles" drop column "first_name";
-
-alter table "public"."user_profiles" drop column "last_name";
-
-
