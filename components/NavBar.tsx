e1a1f48808b8a2bfc55d31c766a97432b4c5f6e9
Luke Hoskins <luke@hoskins.cloud>
2024-02-23 20:20:14 +0000

    Adjust navbar to more easily handle more links
    - convert `searchButton` to a generic `NavLink`
    - Adjust layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,19 @@
 import AuthButton from "./AuthButton";
 import HomeButton from "./HomeButton";
-import SearchButton from "./SearchButton";
+import { NavLink } from "@/components/NavLink";
 
 export default function NavBar() {
-    return(
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-            <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+    return (
+        <nav className="w-full flex-none flex justify-center border-b border-b-foreground/10 h-16">
+            <div className="w-full max-w-[614px] md:max-w-[80%] flex items-center p-3 text-sm">
                 <HomeButton />
-                <SearchButton />
+                <div className="flex-1 flex flex-row px-2 gap-4">
+                    <NavLink 
+                        text="Properties" 
+                        href="/properties"
+                    />
+                    {/* Space for other links here */}
+                </div>
                 <AuthButton />
             </div>
         </nav>
