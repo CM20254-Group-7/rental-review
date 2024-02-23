5d034ca4412f7a8a076bceea1c0bb171ee512ed8
Luke Hoskins <luke@hoskins.cloud>
2024-02-23 21:50:29 +0000

    increase padding of navigation links in header componenti

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
         <nav className="w-full flex-none flex justify-center border-b border-b-foreground/10 h-16">
             <div className="w-full max-w-[614px] md:max-w-[80%] flex items-center p-3 text-sm">
                 <HomeButton />
-                <div className="flex-1 flex flex-row px-2 gap-4">
+                <div className="flex-1 flex flex-row px-8 gap-4">
                     <NavLink 
                         text="Properties" 
                         href="/properties"
