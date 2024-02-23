a451e1ec8a8e2a2f3eda4bcabae1760a50fd38e4
Luke Hoskins <luke@hoskins.cloud>
2024-02-23 20:23:53 +0000

    update footer and add placeholder for a dark mode toggle

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
+import DarkModeToggle from "./DarkModeToggle";
+
 export default function Footer() {
-    return(
-        <div>
-            <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-                <p>This is the footer. <br></br>
-                It will be holding all of our terms and conditions links and the usual footer stuff.</p>
-            </footer>
-        </div>
+    return (
+        <footer className="w-full border-t border-t-foreground/10 p-8 flex flex-col justify-center text-center text-xs">
+            <p>This is the footer.</p>
+            <p>It will be holding all of our terms and conditions links and the usual footer stuff.</p>
+            <DarkModeToggle />
+        </footer>
     )
 }
