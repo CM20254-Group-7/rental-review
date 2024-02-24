2f70b25ecfb77835d8d48b9f74e4bf755924512a
Luke Hoskins <luke@hoskins.cloud>
2024-02-24 18:48:39 +0000

    refactor: extract search and property list into seperate components

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -20,22 +20,34 @@ export default async function PropertiesPage() {
 
     return (
         <div>
-            <form
-                className="animate-in flex-1 flex flex-col w-full justify-center gap-1 text-foreground">
-                <input className="mt-5 rounded-md px-4 py-2 bg-inherit border mb-1"
-                    name="address"
-                    placeholder="13 Argyle Terrace, Staverton, Bath, BA2 3DF..." />
-                <button className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-5">
-                    Search (no functionality yet)
-                </button>
-            </form>
+            <SideBar />
             <ul>
-                {properties?.map((p) => {
-                    return (
-                        <li>{p.address}</li>
-                    )
-                })}
+                <PropertyResults properties={properties} />
             </ul>
         </div>
     )
+}
+
+const SideBar: React.FC = () => {
+    return (
+        <form
+            className="animate-in flex-1 flex flex-col w-full justify-center gap-1 text-foreground">
+            <input className="mt-5 rounded-md px-4 py-2 bg-inherit border mb-1"
+                name="address"
+                placeholder="13 Argyle Terrace, Staverton, Bath, BA2 3DF..." />
+            <button className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-5">
+                Search (no functionality yet)
+            </button>
+        </form>
+    )
+}
+
+const PropertyResults: React.FC<{properties: {
+    address: string;
+}[]}> = ({properties}) => {
+    return properties?.map((property) => {
+        return (
+            <li>{property.address}</li>
+        )
+    })
 }
