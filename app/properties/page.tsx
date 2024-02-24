90925607ccb3ba1b40a983acd1e8cf070ab49f4c
Luke Hoskins <luke@hoskins.cloud>
2024-02-24 18:41:57 +0000

    chore: update code formatting to use consistent spacing

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,17 +1,17 @@
-import {createClient} from '@/utils/supabase/server'
+import { createClient } from '@/utils/supabase/server'
 import { error } from 'console';
 import { cookies } from 'next/headers';
 
-export default async function PropertiesPage(){
+export default async function PropertiesPage() {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
 
-    const{ data:properties, error: propertiesError } = await supabase
+    const { data: properties, error: propertiesError } = await supabase
         .from('properties')
         .select('address')
 
-    if ( propertiesError ) {
-        return(
+    if (propertiesError) {
+        return (
             <div>
                 <main>
                     There is an error with the properties database.
@@ -20,14 +20,14 @@ export default async function PropertiesPage(){
         )
     }
 
-    return(
+    return (
         <div>
             <div>
                 <form
-                className="animate-in flex-1 flex flex-col w-full justify-center gap-1 text-foreground">
-                    <input className="mt-5 rounded-md px-4 py-2 bg-inherit border mb-1" 
-                    name="address" 
-                    placeholder="13 Argyle Terrace, Staverton, Bath, BA2 3DF..."/>
+                    className="animate-in flex-1 flex flex-col w-full justify-center gap-1 text-foreground">
+                    <input className="mt-5 rounded-md px-4 py-2 bg-inherit border mb-1"
+                        name="address"
+                        placeholder="13 Argyle Terrace, Staverton, Bath, BA2 3DF..." />
                     <button className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-5">
                         Search (no functionality yet)
                     </button>
