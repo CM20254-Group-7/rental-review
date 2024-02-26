e9422de5d20911cae443c95b680f1985c15ae603
Luke Hoskins <luke@hoskins.cloud>
2024-02-26 11:38:49 +0000

    fix: make columns nullable in properties table

--- supabase/migrations/20240222212957_added-country-field.sql
+++ supabase/migrations/20240222212957_added-country-field.sql
@@ -1,17 +1,17 @@
-alter table "public"."properties" add column "baths" smallint not null; 
+alter table "public"."properties" add column "baths" smallint; 
 
-alter table "public"."properties" add column "beds" smallint not null; 
+alter table "public"."properties" add column "beds" smallint; 
 
-alter table "public"."properties" add column "country" text not null; 
+alter table "public"."properties" add column "country" text; 
 
 alter table "public"."properties" add column "county" text; 
 
 alter table "public"."properties" add column "description" text; 
 
-alter table "public"."properties" add column "house" text not null; 
+alter table "public"."properties" add column "house" text; 
 
-alter table "public"."properties" add column "postcode" text not null; 
+alter table "public"."properties" add column "postcode" text; 
 
-alter table "public"."properties" add column "property_type" text not null; 
+alter table "public"."properties" add column "property_type" text; 
 
-alter table "public"."properties" add column "street" text not null; 
+alter table "public"."properties" add column "street" text; 
