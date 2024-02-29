5ed2a28f89fe89a2102ec12c55b397d5367be110
OttoChu <96123119+OttoChu@users.noreply.github.com>
2024-02-29 18:59:09 +0000

    more formating

--- playwright-tests/create-review-page-tests.spec.ts
+++ playwright-tests/create-review-page-tests.spec.ts
@@ -13,8 +13,8 @@ test.describe('Anon User Tests', () => {
 
     // checks Anon receives relevant message
     await expect(page.locator('form')).toContainText('User Not Logged In')
-  })
-})
+  });
+});
 
 // TODO: fill in params for createReviewPage() once it has been implemented
 test.describe('User 1 tests', () => {
@@ -24,7 +24,7 @@ test.describe('User 1 tests', () => {
   test('reviewing without selecting a property', async ({ page }) => {
     test.fixme();
 
-    await page.goto('./reviews/create/')
+    await page.goto('./reviews/create/');
 
     // fills in date
     await page.getByLabel('Review Date').fill(`${dateTime.getDate()}`);
@@ -41,59 +41,59 @@ test.describe('User 1 tests', () => {
 
     // checks for no property selected message
     await expect(page.locator('form')).toContainText('No Property Selected');
-    })
+  });
 
     // This is not implemented yet, so not tested
-    test('reviewing new property as User 1', async ({ page }) => {
-      // TODO: do full workflow to get to new property review page
-      test.fixme();
+  test('reviewing new property as User 1', async ({ page }) => {
+    // TODO: do full workflow to get to new property review page
+    test.fixme();
 
-      // fills in date
-      await page.getByLabel('Review Date').fill(`${dateTime.getDate()}`);
+    // fills in date
+    await page.getByLabel('Review Date').fill(`${dateTime.getDate()}`);
 
-      // fills in review contents
-      await page.getByLabel('Review Body').fill('test review');
+    // fills in review contents
+    await page.getByLabel('Review Body').fill('test review');
 
-      // gives review to proeperty and landlord
-      await page.getByLabel('Property Rating').fill('5');
-      await page.getByLabel('Landlord Rating').fill('5');
+    // gives review to proeperty and landlord
+    await page.getByLabel('Property Rating').fill('5');
+    await page.getByLabel('Landlord Rating').fill('5');
 
-      // clicks "Create Review" button
-      await page.getByRole('button', { name: 'Create Review' }).click();
+    // clicks "Create Review" button
+    await page.getByRole('button', { name: 'Create Review' }).click();
 
-      // checks for review created message
-      await expect(page.locator('form')).toContainText('Review Created');
-    })
+    // checks for review created message
+    await expect(page.locator('form')).toContainText('Review Created');
+  });
 
     // reviews page not implemented, so .../create/... returns 404 error
-    test('reviewing existing property as User 1', async ({ page }) => {
-      test.fixme();
+  test('reviewing existing property as User 1', async ({ page }) => {
+    test.fixme();
 
-      // Goes to the createReview page of a pre-existing property
-      await page.goto('./reviews/create/1ececec8-4bbf-445f-8de0-f563caf0bf01')
+    // Goes to the createReview page of a pre-existing property
+    await page.goto('./reviews/create/1ececec8-4bbf-445f-8de0-f563caf0bf01');
 
-      // fills in date
-      await page.getByLabel('Review Date').fill(`${dateTime.getDate()}`);
+    // fills in date
+    await page.getByLabel('Review Date').fill(`${dateTime.getDate()}`);
 
-      // fills in review contents
-      await page.getByLabel('Review Body').fill('test review');
+    // fills in review contents
+    await page.getByLabel('Review Body').fill('test review');
 
-      // gives review to proeperty and landlord
-      await page.getByLabel('Property Rating').fill('5');
-      await page.getByLabel('Landlord Rating').fill('5');
+    // gives review to proeperty and landlord
+    await page.getByLabel('Property Rating').fill('5');
+    await page.getByLabel('Landlord Rating').fill('5');
 
-      // clicks "Create Review" button
-      await page.getByRole('button', { name: 'Create Review' }).click();
+    // clicks "Create Review" button
+    await page.getByRole('button', { name: 'Create Review' }).click();
 
-      // checks for existing property message
-      await expect(page.locator('form')).toContainText('Property Already Exists');
-    })
+    // checks for existing property message
+    await expect(page.locator('form')).toContainText('Property Already Exists');
+  });
 
   test('reviewing property already reviewd by User 1 as User 1', async ({ page }) => {
     test.fixme();
 
     // TODO: change propertyId to one that User 1 made
-    await page.goto('./reviews/create/1ececec8-4bbf-445f-8de0-f563caf0bf01')
+    await page.goto('./reviews/create/1ececec8-4bbf-445f-8de0-f563caf0bf01');
 
     // fills in date
     await page.getByLabel('Review Date').fill(`${dateTime.getDate()}`);
@@ -110,5 +110,5 @@ test.describe('User 1 tests', () => {
 
     // checks for existing user review message
     await expect(page.locator('form')).toContainText('User has already reviewed this property');
-  })
-})
+  });
+});
