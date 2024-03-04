addd1b59ab62794fee19c9930fa18550c3444103
Ailam7 <mo.ailam777@gmail.com>
2024-03-04 11:04:02 +0000

    Added temporary dashboard link to NavBar (modify later to show only when authenticated)

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -4,13 +4,14 @@ import AuthButton from './AuthButton';
 import HomeButton from './HomeButton';
 
 const NavBar: React.FC = () => (
-  <nav className='w-full flex-none flex justify-center border-b shadow-md bg-primary/10 shadow-primary/10 h-16'>
-    <div className='w-full max-w-[614px] md:max-w-[80%] flex items-center p-3 text-sm'>
+  <nav className="w-full flex-none flex justify-center border-b shadow-md bg-primary/10 shadow-primary/10 h-16">
+    <div className="w-full max-w-[614px] md:max-w-[80%] flex items-center p-3 text-sm">
       <HomeButton />
-      <div className='flex-1 flex flex-row px-8 gap-4'>
+      <div className="flex-1 flex flex-row px-8 gap-4">
+        <NavLink text="Properties" href="/properties" />
         <NavLink
-          text='Properties'
-          href='/properties'
+          text="Dashboard"
+          href="/dashboard" // Remember to change later to appear only when authenticated
         />
         {/* Space for other links here */}
       </div>
