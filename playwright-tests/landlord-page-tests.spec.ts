e02907755d4bf1f84cef927776f5c82a8c7a8345
Otto Chu <96123119+OttoChu@users.noreply.github.com>
2024-03-05 13:12:20 +0000

    RRS-133 display average landlord rating on their profile (#62)
    * Fixed the property rating
    
    * Started to format the landlord page
    
    * Added average rating

--- playwright-tests/landlord-page-tests.spec.ts
+++ playwright-tests/landlord-page-tests.spec.ts
@@ -7,37 +7,85 @@ const firstUser = users[0]; // Landlord with 2 properties
 const secondUser = users[1]; // Landlord with no properties
 const thirdUser = users[2]; // Not a landlord
 
-test.describe('Landlord Profile Page Tests', () => {
-  test(`${secondUser.label} - No owned properties`, async ({ page }) => {
-    await page.goto(`http://localhost:3000/profiles/${secondUser.id}`);
-    await expect(page.getByRole('main')).toContainText(`${secondUser.landlordProfile!.displayName}`);
-    await expect(page.getByRole('main')).toContainText(`${secondUser.landlordProfile!.displayEmail}`);
-    await expect(page.getByRole('main')).toContainText(`${secondUser.landlordProfile!.userBio}`);
-
-    await expect(page.getByRole('main')).toContainText('No properties');
-  });
-
-  test(`${firstUser.label} - Two owned properties`, async ({ page }) => {
-    await page.goto(`http://localhost:3000/profiles/${firstUser.id}`);
-    await expect(page.getByRole('main')).toContainText(`${firstUser.landlordProfile!.displayName}`);
-    await expect(page.getByRole('main')).toContainText(`${firstUser.landlordProfile!.displayEmail}`);
-    await expect(page.getByRole('main')).toContainText(`${firstUser.landlordProfile!.userBio}`);
-    await expect(page.getByRole('list')).toContainText(`${firstUser.landlordProfile!.properties[0]}`);
-    await expect(page.getByRole('list')).toContainText(`${firstUser.landlordProfile!.properties[1]}`);
-  });
-
-  test(`${thirdUser.label} - Not a landlord`, async ({ page }) => {
-    await page.goto(`http://localhost:3000/profiles/${thirdUser.id}`);
-    await expect(page.locator('h2')).toContainText('This page could not be found.');
-  });
-
-  test('Landlord does not exist: no user with ID', async ({ page }) => {
-    await page.goto('http://localhost:3000/profile/123');
-    await expect(page.locator('h2')).toContainText('This page could not be found.');
-  });
-
-  test('LandlordId not provided test', async ({ page }) => {
+test.describe('Landlord profile landing page tests', () => {
+  test('Contains landlords', async ({ page }) => {
+    test.fixme();
+    // Need to get the profile landing page done first
     await page.goto('http://localhost:3000/profiles');
     await expect(page.getByRole('main')).toContainText('Profile Page');
+  });
+});
+
+test.describe('Landlord profile details page tests', () => {
+  test.describe('Wrong landlordID tests', () => {
+    test(`${thirdUser.label} - Not a landlord`, async ({ page }) => {
+      await page.goto(`http://localhost:3000/profiles/${thirdUser.id}`);
+      await expect(page.locator('h2')).toContainText('This page could not be found.');
+    });
+
+    test('Landlord does not exist: no user with ID', async ({ page }) => {
+      await page.goto('http://localhost:3000/profile/123');
+      await expect(page.locator('h2')).toContainText('This page could not be found.');
+    });
+
+    test('LandlordId not provided test', async ({ page }) => {
+      test.fixme();
+      // Need to get the profile landing page done first
+      await page.goto('http://localhost:3000/profiles');
+      await expect(page.getByRole('main')).toContainText('Profile Page');
+    });
+  });
+
+  test.describe('Landlord details tests', () => {
+    test.describe('Landlord personal details tests', () => {
+      test.describe('Contains correct display name', () => {
+        test(`${firstUser.label} has correct name`, async ({ page }) => {
+          await page.goto(`http://localhost:3000/profiles/${firstUser.id}`);
+          await expect(page.getByRole('main')).toContainText(`${firstUser.landlordProfile!.displayName}`);
+        });
+
+        test(`${secondUser.label} has correct name`, async ({ page }) => {
+          await page.goto(`http://localhost:3000/profiles/${secondUser.id}`);
+          await expect(page.getByRole('main')).toContainText(`${secondUser.landlordProfile!.displayName}`);
+        });
+      });
+
+      test.describe('Contains correct email', () => {
+        test(`${firstUser.label} has correct email`, async ({ page }) => {
+          await page.goto(`http://localhost:3000/profiles/${firstUser.id}`);
+          await expect(page.getByRole('main')).toContainText(`${firstUser.landlordProfile!.displayEmail}`);
+        });
+
+        test(`${secondUser.label} has correct email`, async ({ page }) => {
+          await page.goto(`http://localhost:3000/profiles/${secondUser.id}`);
+          await expect(page.getByRole('main')).toContainText(`${secondUser.landlordProfile!.displayEmail}`);
+        });
+      });
+
+      test.describe('Contains correct bio', () => {
+        test(`${firstUser.label} has correct bio`, async ({ page }) => {
+          await page.goto(`http://localhost:3000/profiles/${firstUser.id}`);
+          await expect(page.getByRole('main')).toContainText(`${firstUser.landlordProfile!.userBio}`);
+        });
+
+        test(`${secondUser.label} has correct bio`, async ({ page }) => {
+          await page.goto(`http://localhost:3000/profiles/${secondUser.id}`);
+          await expect(page.getByRole('main')).toContainText(`${secondUser.landlordProfile!.userBio}`);
+        });
+      });
+    });
+
+    test.describe('Landlord owned properties tests', () => {
+      test(`${secondUser.label} - No owned properties`, async ({ page }) => {
+        await page.goto(`http://localhost:3000/profiles/${secondUser.id}`);
+        await expect(page.getByRole('main')).toContainText('No properties');
+      });
+
+      test(`${firstUser.label} - Two owned properties`, async ({ page }) => {
+        await page.goto(`http://localhost:3000/profiles/${firstUser.id}`);
+        await expect(page.getByRole('list')).toContainText(`${firstUser.landlordProfile!.properties[0]}`);
+        await expect(page.getByRole('list')).toContainText(`${firstUser.landlordProfile!.properties[1]}`);
+      });
+    });
   });
 });
