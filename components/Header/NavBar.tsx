e45fd366315d7cf5d2587744a02a7f53cd991ddb
Otto Chu <96123119+OttoChu@users.noreply.github.com>
2024-03-14 23:25:34 +0000

    RRS-23 display dashboard button only while logged in (#77)
    * Only show dashboard button if logged in
    
    * Remove useless comment
    
    * fixed lint

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -1,30 +1,43 @@
+import createClient from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
 import NavLink from '@/components/Header/NavLink';
 import React from 'react';
 import AuthButton from './AuthButton';
 import HomeButton from './HomeButton';
 import Help from './Help';
 
-const NavBar: React.FC = () => (
-  <nav className='w-full flex-none flex justify-center border-b shadow-md bg-primary/10 shadow-primary/10 h-16'>
-    <div className='w-full max-w-[614px] md:max-w-[80%] flex items-center p-3 text-sm'>
+const NavBar: React.FC = async () => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: { user }, error: userError } = await supabase.auth.getUser();
+  let loggedIn = true;
+  if (userError || !user) {
+    loggedIn = false;
+  }
 
-      <HomeButton />
-      <div className='flex-1 flex flex-row px-8 gap-4'>
-        <NavLink
-          text='Dashboard'
-          href='/dashboard' // Remember to change later to appear only when authenticated
-        />
-        <NavLink text='Properties' href='/properties' />
-        <NavLink
-          text='Landlords'
-          href='/profiles'
-        />
-        {/* Space for other links here */}
+  return (
+    <nav className='w-full flex-none flex justify-center border-b shadow-md bg-primary/10 shadow-primary/10 h-16'>
+      <div className='w-full max-w-[614px] md:max-w-[80%] flex items-center p-3 text-sm'>
+        <HomeButton />
+        <div className='flex-1 flex flex-row px-8 gap-4'>
+          {loggedIn && (
+            <NavLink
+              text='Dashboard'
+              href='/dashboard'
+            />
+          )}
+          <NavLink text='Properties' href='/properties' />
+          <NavLink
+            text='Landlords'
+            href='/profiles'
+          />
+          {/* Space for other links here */}
+        </div>
+        <AuthButton />
+        <Help />
       </div>
-      <AuthButton />
-      <Help />
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default NavBar;
