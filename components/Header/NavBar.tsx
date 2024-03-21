979e2686af2830bbaac9f68d7bf0a1e05bfdf5e0
Luke Hoskins <luke@hoskins.cloud>
2024-03-21 19:05:30 +0000

    dashboard link last to minimise layout shift

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -20,17 +20,17 @@ const NavBar: React.FC = async () => {
       <div className='w-full max-w-[614px] md:max-w-[80%] flex items-center p-3 text-sm'>
         <HomeButton />
         <div className='flex-1 flex flex-row px-8 gap-4'>
+          <NavLink text='Properties' href='/properties' />
+          <NavLink
+            text='Landlords'
+            href='/profiles'
+          />
           {loggedIn && (
             <NavLink
               text='Dashboard'
               href='/dashboard'
             />
           )}
-          <NavLink text='Properties' href='/properties' />
-          <NavLink
-            text='Landlords'
-            href='/profiles'
-          />
           {/* Space for other links here */}
         </div>
         <AuthButton />
