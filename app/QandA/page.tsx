b948e92d475ac8995c0d0fd819b3ae55391b0ff6
OttoChu <96123119+OttoChu@users.noreply.github.com>
2024-04-01 15:10:28 +0100

    Changed the content of the FAQ page

--- app/QandA/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-
-const QandAPage: NextPage = () => (
-  <div>
-    Hello! Unfortunately, no one&apos;s asked us any questions, so...
-  </div>
-);
-
-export default QandAPage;
