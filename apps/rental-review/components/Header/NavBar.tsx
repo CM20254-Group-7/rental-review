dfe70b8b355ce29b22105ccecc150eee869f386f
Luke Hoskins <luke@hoskins.cloud>
2024-04-19 09:47:21 +0100

    responsive top nav bar

--- apps/rental-review/components/Header/NavBar.tsx
+++ apps/rental-review/components/Header/NavBar.tsx
@@ -1,11 +1,24 @@
-import { createServerSupabaseClient } from '@repo/supabase-client-helpers/server-only';
-import NavLink from '@/components/Header/NavLink';
-import React from 'react';
-import AuthButton from './AuthButton';
-import HomeButton from './HomeButton';
-import Help from './Help';
+import Link from 'next/link';
+import { CircleUser, Home, Menu, Package2, Search } from 'lucide-react';
 
-const NavBar: React.FC = async () => {
+import { TextInput as Input, Button } from '@/components/ClientTremor';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/sheet';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/dropdown';
+import HomeButton from './HomeButton';
+import NavLink from './NavLink';
+import { FC } from 'react';
+import AuthButton from './AuthButton';
+import Help from './Help';
+import { createServerSupabaseClient } from '@repo/supabase-client-helpers/server-only';
+
+const NavBar: FC = async () => {
   const supabase = createServerSupabaseClient();
   const {
     data: { user },
@@ -15,21 +28,73 @@ const NavBar: React.FC = async () => {
   if (userError || !user) {
     loggedIn = false;
   }
-
   return (
-    <nav className='bg-primary/10 shadow-primary/10 flex h-16 w-full flex-none justify-center border-b shadow-md'>
-      <div className='flex w-full max-w-[614px] items-center p-3 text-sm md:max-w-[80%]'>
-        <HomeButton />
-        <div className='flex flex-1 flex-row gap-4 px-8'>
+    <header className='bg-background sticky top-0 h-16 w-screen'>
+      <div className='bg-primary/10 grid h-full w-full grid-cols-3 grid-rows-1 items-center gap-4 border-b px-4 md:grid-cols-1 md:px-6'>
+        <nav className='hidden w-full flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
+          <HomeButton />
           <NavLink text='Properties' href='/properties' />
           <NavLink text='Landlords' href='/profiles' />
           {loggedIn && <NavLink text='Dashboard' href='/dashboard' />}
-          {/* Space for other links here */}
+          <div className='flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4'>
+            <AuthButton />
+            <Help />
+            {/* <form className='ml-auto flex-1 sm:flex-initial'>
+            <div className='relative'>
+              <Search className='text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4' />
+              <Input
+                type='text'
+                placeholder='Search products...'
+                className='pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]'
+              />
+            </div>
+          </form>
+          `<DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant='secondary' className='rounded-full'>
+                <CircleUser className='h-5 w-5' />
+                <span className='sr-only'>Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align='end'>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu> */}
+          </div>
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant='secondary' className='w-fit shrink-0 md:hidden'>
+              <Menu className='h-5 w-5' />
+              <span className='sr-only'>Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            side='left'
+            className='flex w-screen flex-col justify-between'
+          >
+            <nav className='grid place-items-center gap-6 text-lg font-medium'>
+              <HomeButton />
+              <NavLink text='Properties' href='/properties' />
+              <NavLink text='Landlords' href='/profiles' />
+              {loggedIn && <NavLink text='Dashboard' href='/dashboard' />}
+            </nav>
+            <div className='grid place-items-center gap-6 py-6 text-lg font-medium'>
+              <AuthButton />
+              <Help />
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className='flex flex-1 justify-center md:hidden'>
+          <HomeButton />
         </div>
-        <AuthButton />
-        <Help />
       </div>
-    </nav>
+    </header>
   );
 };
 
--- apps/rental-review/components/Header/UserManagementButton.tsx
+++ apps/rental-review/components/Header/UserManagementButton.tsx
@@ -2,73 +2,53 @@
 
 import { UserCircleIcon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import signOut from './actions';
-
-// define hook to handle click outside of component
-const useComponentVisible = () => {
-  const [isComponentVisible, setIsComponentVisible] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (ref.current && !ref.current.contains(event.target as Element)) {
-      setIsComponentVisible(!isComponentVisible);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside, !isComponentVisible);
-
-    return () => {
-      document.removeEventListener(
-        'click',
-        handleClickOutside,
-        !isComponentVisible,
-      );
-    };
-  });
-
-  return { ref, isComponentVisible, setIsComponentVisible };
-};
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../dropdown';
+import { Button } from '../ClientTremor';
 
 const UserManagementButton: React.FC<{ email: string }> = ({ email }) => {
-  const { ref, isComponentVisible, setIsComponentVisible } =
-    useComponentVisible();
-
   return (
-    <div className='relative'>
-      <button
-        type='button'
-        className={`bg-background hover:bg-secondary/10 hover:shadow-primary/20 text-accent flex flex-row items-center justify-center gap-2 border px-3 py-2 text-sm transition-all hover:shadow-sm ${isComponentVisible ? 'rounded-t-md' : 'rounded-md'}`}
-        onClick={() => setIsComponentVisible(true)}
-      >
-        <p>{email}</p>
-        <UserCircleIcon className='h-6 w-6' />
-      </button>
-
-      {isComponentVisible && (
-        <div
-          ref={ref}
-          className='bg-background hover:shadow-primary/20 text-foreground absolute top-[100%] flex w-full flex-col items-center justify-center rounded-b-md border-x border-b text-sm transition-all hover:shadow-sm'
-        >
-          <Link
-            href='/account'
-            className='hover:bg-secondary/10 w-full border-b px-4 py-2 text-right no-underline'
-          >
-            Manage Account
-          </Link>
-
-          <form action={signOut} className='contents'>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='secondary' className='rounded-full'>
+          <div className='text-accent mid:gap-2 flex flex-row items-center'>
+            <p className='mid:flex md:hidden'>{email}</p>
+            <UserCircleIcon className='h-6 w-6' />
+            {/* <CircleUser className='h-5 w-5' /> */}
+          </div>
+          <span className='sr-only'>Toggle user menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='bg-background min-w-full p-0' asChild>
+        <form action={signOut}>
+          {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
+          {/* <DropdownMenuSeparator /> */}
+          <DropdownMenuItem asChild>
+            <Link
+              href='/account'
+              className='focus:bg-secondary/10 w-full border-b px-4 py-2 text-right no-underline'
+            >
+              Manage Account
+            </Link>
+          </DropdownMenuItem>
+          {/* <DropdownMenuSeparator /> */}
+          <DropdownMenuItem asChild>
             <button
               type='submit'
-              className='hover:bg-secondary/10 w-full rounded-b-md px-4 py-2 text-right no-underline'
+              className='focus:bg-secondary/10 flex w-full justify-end border-b px-4 py-2 text-right no-underline'
             >
               Logout
             </button>
-          </form>
-        </div>
-      )}
-    </div>
+          </DropdownMenuItem>
+        </form>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
