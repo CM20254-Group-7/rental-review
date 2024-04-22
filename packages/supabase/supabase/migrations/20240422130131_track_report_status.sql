f4aaf64a10a2e1855aa112dfd43f21cd8eb16898
Luke Hoskins <luke@hoskins.cloud>
2024-04-22 14:20:11 +0100

    hide restricted reviews from results

--- packages/supabase/supabase/migrations/20240422130131_track_report_status.sql
+++ packages/supabase/supabase/migrations/20240422130131_track_report_status.sql
@@ -5,3 +5,14 @@ alter table "public"."review_reports" add column "moderator_comment" report_stat
 alter table "public"."review_reports" add column "status" report_status not null default 'reported'::report_status;
 
 alter table "public"."reviews" add column "restricted" boolean not null default false;
+
+alter policy "Enable read access for all users"
+on "public"."reviews"
+rename to "Enable read access to non-restricted reviews for all users";
+
+alter policy "Enable read access to non-restricted reviews for all users"
+on "public"."reviews"
+to public
+using (
+  (not restricted)
+);
